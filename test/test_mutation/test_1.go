b299bbc7c2741fe2f43262c42c75cffecd978e13
lascyb <37490770+lascyb@users.noreply.github.com>
2026-01-08 16:24:39 +0800

    更新了测试示例

--- test/test_mutation/test_1.go
+++ test/test_mutation/test_1.go
@@ -6,26 +6,43 @@ import (
 	graphql "github.com/lascyb/struct-to-graphql"
 )
 
-type Foo struct {
-	Bar string `graphql:"bar"`
+type Product struct {
+	Id string `json:"id" graphql:"id"`
 }
 type ProductVariant struct {
 	ID    string `json:"id" graphql:"id"`
+	Sku   string `json:"sku" graphql:"sku"`
 	Price string `json:"price" graphql:"price"`
 }
-type Mutation struct {
-	ProductVariantsBulkUpdate struct {
-		Product struct {
-			ID string `json:"id" graphql:"id"`
-		} `graphql:"product"`
-		ProductVariants []ProductVariant `json:"productVariants" graphql:"productVariants"`
-	} `graphql:"productVariantsBulkUpdate(productId:$productId:ID!,variants: $variants:[ProductVariantsBulkInput!]!)"`
+type ProductVariantsBulkUpdate struct {
+	Product         Product          `json:"product" graphql:"product"`
+	ProductVariants []ProductVariant `json:"productVariants" graphql:"productVariants"`
+}
+
+type ProductVariantsBulkUpdatePayload struct {
+	// 注意参数定义顺序，参数名:$变量名:变量类型
+	ProductVariantsBulkUpdate ProductVariantsBulkUpdate `json:"productVariantsBulkUpdate" graphql:"productVariantsBulkUpdate(productId:$productId:ID!,variants:$variants:[ProductVariantsBulkInput!]!)"`
 }
 
 func main() {
-	q, _ := graphql.Marshal(Mutation{})
+	q, _ := graphql.Marshal(ProductVariantsBulkUpdatePayload{})
 
 	// 获取完整查询
-	query, _ := q.Mutation("productVariantsBulkUpdate")
+	query, _ := q.Mutation("UpdateProductVariantsOptionValuesInBulk")
 	fmt.Println(query)
+	//输出结果
+	/**
+	mutation UpdateProductVariantsOptionValuesInBulk($productId: ID!, $variants: [ProductVariantsBulkInput!]!) {
+	  productVariantsBulkUpdate(productId: $productId, variants: $variants){
+	    product{
+	      id
+	    }
+	    productVariants{
+	      id
+	      sku
+	      price
+	    }
+	  }
+	}
+	*/
 }
