25f9dae18cfa69964224a436fe46893b2d374ad4
lascyb <37490770+lascyb@users.noreply.github.com>
2026-01-08 16:59:03 +0800

    更新了测试实例

--- test/test_mutation/productVariantsBulkUpdate_test.go
+++ test/test_mutation/productVariantsBulkUpdate_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"testing"
 
 	graphql "github.com/lascyb/struct-to-graphql"
 )
@@ -19,30 +20,31 @@ type ProductVariantsBulkUpdate struct {
 	ProductVariants []ProductVariant `json:"productVariants" graphql:"productVariants"`
 }
 
-type ProductVariantsBulkUpdatePayload struct {
+// ProductVariantsBulkUpdatePayload 生成Mutation的同时，如果定义了正确的 json tag，就可以用来接受请求响应
+type Mutation struct {
 	// 注意参数定义顺序，参数名:$变量名:变量类型
 	ProductVariantsBulkUpdate ProductVariantsBulkUpdate `json:"productVariantsBulkUpdate" graphql:"productVariantsBulkUpdate(productId:$productId:ID!,variants:$variants:[ProductVariantsBulkInput!]!)"`
 }
 
-func main() {
-	q, _ := graphql.Marshal(ProductVariantsBulkUpdatePayload{})
+func Test_productVariantsBulkUpdate(test *testing.T) {
+	q, _ := graphql.Marshal(Mutation{})
 
 	// 获取完整查询
-	query, _ := q.Mutation("UpdateProductVariantsOptionValuesInBulk")
+	query, _ := q.Mutation("MyMutation")
 	fmt.Println(query)
-	//输出结果
-	/**
-	mutation UpdateProductVariantsOptionValuesInBulk($productId: ID!, $variants: [ProductVariantsBulkInput!]!) {
-	  productVariantsBulkUpdate(productId: $productId, variants: $variants){
-	    product{
-	      id
-	    }
-	    productVariants{
-	      id
-	      sku
-	      price
-	    }
-	  }
-	}
+	//输出结果 productVariantsBulkUpdate.graphql
+	/*
+		mutation UpdateProductVariantsOptionValuesInBulk($productId: ID!, $variants: [ProductVariantsBulkInput!]!) {
+		    productVariantsBulkUpdate(productId: $productId, variants: $variants){
+		        product{
+		            id
+		        }
+		        productVariants{
+		            id
+		            sku
+		            price
+		        }
+		    }
+		}
 	*/
 }
---
+++ test/test_query/discountNodes_test.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	graphql "github.com/lascyb/struct-to-graphql"
+)
+
+type DiscountCodeBasic struct {
+	Title   string `json:"title" graphql:"title"`
+	Summary string `json:"summary" graphql:"summary"`
+	Status  string `json:"status" graphql:"status"`
+}
+type DiscountAutomaticBasic struct {
+	Title   string `json:"title" graphql:"title"`
+	Summary string `json:"summary" graphql:"summary"`
+	Status  string `json:"status" graphql:"status"`
+}
+type DiscountCodeBxgy struct {
+	Title   string `json:"title" graphql:"title"`
+	Summary string `json:"summary" graphql:"summary"`
+	Status  string `json:"status" graphql:"status"`
+}
+type Discount struct {
+	Typename string `json:"__typename" graphql:"__typename,union"` //联合类型必须使用 __typename,union 进行标记，否则会识别成内联字段
+	DiscountCodeBasic
+	DiscountAutomaticBasic
+	DiscountCodeBxgy
+}
+
+type DiscountNode struct {
+	Discount Discount `json:"discount" graphql:"discount"`
+	Id       string   `json:"id" graphql:"id"`
+}
+type PageInfo struct {
+	EndCursor       string `json:"endCursor" graphql:"endCursor"`
+	HasNextPage     bool   `json:"hasNextPage" graphql:"hasNextPage"`
+	HasPreviousPage bool   `json:"hasPreviousPage" graphql:"hasPreviousPage"`
+	StartCursor     string `json:"startCursor" graphql:"startCursor"`
+}
+type DiscountNodeConnection struct {
+	Nodes    []DiscountNode `json:"nodes" graphql:"nodes"`
+	PageInfo PageInfo       `json:"pageInfo" graphql:"pageInfo"`
+}
+type Query struct {
+	DiscountNodeConnection DiscountNodeConnection `json:"discountNodes" graphql:"discountNodes(query:$query:String!, first: 10)"`
+}
+
+func Test_discountNodes(test *testing.T) {
+	q, _ := graphql.Marshal(Query{})
+
+	// 获取完整查询
+	query, _ := q.Query("MyQuery")
+
+	fmt.Println(query)
+
+	//输出结果 discountNodes.graphql
+	/*
+		query MyQuery($query: String!) {
+		    discountNodes(first: 10, query: $query){
+		        nodes{
+		            discount{
+		                __typename
+		                ... on DiscountCodeBasic {
+		                    title
+		                    summary
+		                    status
+		                }
+		                ... on DiscountAutomaticBasic {
+		                    title
+		                    summary
+		                    status
+		                }
+		                ... on DiscountCodeBxgy {
+		                    title
+		                    summary
+		                    status
+		                }
+		            }
+		            id
+		        }
+		        pageInfo{
+		            endCursor
+		            hasNextPage
+		            hasPreviousPage
+		            startCursor
+		        }
+		    }
+		}
+	*/
+}
